2ccaff1af688bbdc1b4c72bb1c82457bd8fc5dc9
soyamilk <271450260@qq.com>
2025-12-26 17:13:59 +0800

    添加砍树功能

---
+++ scenes/components/hurt_component.gd
@@ -0,0 +1,13 @@
+class_name HurtComponent
+extends Area2D
+
+@export var tool: DataTypes.Tools = DataTypes.Tools.NONE
+
+signal hurt(damage: int)
+
+
+func _on_area_entered(area: Area2D) -> void:
+	var hit_component = area as HitComponent
+
+	if tool == hit_component.current_tool:
+		hurt.emit(hit_component.hit_damage)
